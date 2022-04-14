cf672dcbc2bf5cbc56afdcb2e83c29ba68cfa29f
Rafael de Souza Costa <rafaeldesouzacosta.dev@gmail.com>
2022-04-14 00:54:30 -0300

    atualização

---
+++ src/coelho_da_pascoa/Informacoes.java
@@ -0,0 +1,64 @@
+ // Existem 3 fabricas - Nestle, Garoto, Lacta
+
+//Nestre - Alpino 337g,  Classic 185g, Kit Kat 227g
+
+// #Alpino 337g
+                //Informações Nutricionais:
+                //Porção de 25g (1/14 do ovo)
+                //Valor Energético 134kcal (7%)
+                //Carboidratos 13g (4%)
+                //Proteínas 2,4g (3%)
+                //Gorduras Totais 8,0g (15%)
+                //Gorduras Saturadas 4,7g (21%)
+                //Gorduras Trans 0g (**)
+                //Fibra Alimentar 0,7g (3%)
+                //Sódio 26mg (1%)
+
+//Classic 185g
+                //Informaçao Nutricional    
+                //Porção 25 g (4 quadradinhos)    
+                //Valor Energético    134 kcal = 563 kJ    (7%)
+                //Carboidratos    15 g    (5%)
+                //Proteínas      1,3 g    (2%)
+                //Gorduras Totais    7,6 g    (14%)
+                //Gorduras Saturadas     4,3 g    (20%)
+                //Gorduras Trans    0 g    
+                //Fibra Alimentar    0 g 
+                //Sódio    17 mg    (1%)
+
+//Kit Kat 227g
+               // Informaçao Nutricional   
+                //Porção 25g (1/8 do ovo) 
+                //Valor Energético    136kcal (7%)
+                //Carboidratos    13g (4%)
+                //Proteínas      2,0g (3%)
+                //Gorduras Totais    8,3g (15%)
+                //Gorduras Saturadas     4,9g (22%)
+                //Gorduras Trans    0g
+                //Fibra Alimentar    0g
+                //Sódio    22mg (1%)
+                
+
+//Garoto -  Crocante 215g, Talento 350g, Baton ao leite 172g
+
+//Crocante 215g
+                 // Açúcares	14g
+                //Carboidratos	14g
+                //Fibra Alimentar	0.9g	
+                //Gorduras Saturadas	4.4g	
+                //Gorduras Totais	7.8g	
+                //Gorduras Trans	0g	
+                //Proteínas	1.8g	
+                //Sódio	23mg	
+                //Valor energético	133kcal	
+
+//Talento 350g
+        //Altura do Produto (cm) 31
+        //Largura do produto (cm) 11.6
+        //Peso Bruto 375
+        //Peso Líquido 350
+        //Profundidade do Produto (cm) 11.6
+        //Unidade de Medida do Peso Bruto G
+
+
+//Lacta -  A leite 170g, Oreo 257g, Sonho de Valsa 277g
